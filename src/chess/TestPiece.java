a797120882e7798f315f8a91a82e1ee8ea1e5d31
Matthew Mahnke <mmahnke18@gmail.com>
2014-03-13 23:50:31 -0500

    Updated TestPiece.getMoceLocations()

--- src/chess/TestPiece.java
+++ src/chess/TestPiece.java
@@ -1,27 +1,31 @@
 package chess;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.ImageIcon;
 
 public class TestPiece extends Piece {
 
 	private ImageIcon pieceIcon = new ImageIcon("knight_black.png");
-	
+
 	public TestPiece(int row, int col) {
 		super(row, col);
 	}
-	public ImageIcon getIcon(){
+
+	public ImageIcon getIcon() {
 		return pieceIcon;
 	}
+
 	@Override
-	public List<Location> getMoveLocations() {
+	public List<List<Location>> getMoveLocations() {
 		// TODO Auto-generated method stub
 		List<Location> b = new ArrayList<Location>();
-		b.add(new Location(0,0));
-		return b;
+		b.add(new Location(0, 0));
+		List<List<Location>> a = new ArrayList<List<Location>>();
+		a.add(b);
+		return a;
 	}
+
 	@Override
 	public String toString() {
 		return "TestPiece [pieceIcon=" + pieceIcon + "]";
