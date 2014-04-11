745e09d4d4d3e1a08bc2a22efdb3b4a6e8e971b0
Matthew Mahnke <mmahnke18@gmail.com>
2014-04-10 21:51:15 -0500

    Cleaned up King to use previously implemented methods

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,82 +1,67 @@
 package chess.pieces;
 
+import chess.Location;
+
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.ImageIcon;
-
-import chess.Location;
-
 public class King extends Piece {
 
-	private ImageIcon icon;
-	private char team;
+   public King(int row, int col, char team) {
+      super(row, col);
+      this.team = team;
+      switch(team) {
+         case 'b':
+            icon = new ImageIcon("images/king_black.png");
+            break;
+         case 'w':
+            icon = new ImageIcon("images/king_white.png");
+            break;
+         default:
+            throw new IllegalArgumentException("Team must be 'b' for black team or 'w' for white team.");
+      }
+   }
 
-	public King(int row, int col, char team) {
-		super(row, col);
-		this.team = team;
-		switch (team) {
-		case 'b':
-			icon = new ImageIcon("images/king_black.png");
-			break;
-		case 'w':
-			icon = new ImageIcon("images/king_white.png");
-			break;
-		default:
-			throw new IllegalArgumentException(
-					"Team must be 'b' for black team or 'w' for white team.");
-		}
-	}
+   @Override
+   public List<List<Location>> getMoveLocations() {
+      List<List<Location>> moveLocs = new ArrayList<>();
+      System.out.println(this.getRow() + "" + this.getCol());
 
-	@Override
-	public ImageIcon getIcon() {
-		// TODO Auto-generated method stub
-		return icon;
-	}
 
-	@Override
-	public List<List<Location>> getMoveLocations() {
-		List<List<Location>> moveLocs = new ArrayList<>();
-		System.out.println(this.getRow() + ""+ this.getCol());
-		List<Location> north = new ArrayList<Location>();
-		
-		north.add(new Location(this.getRow()-1,this.getCol()));
-		moveLocs.add(north);
-		
-		List<Location> south = new ArrayList<Location>();
-		south.add(new Location(this.getRow()+1,this.getCol()));
-		moveLocs.add(south);
-		
-		List<Location> east = new ArrayList<Location>();
-		east.add(new Location(this.getRow(),this.getCol()+1));
-		moveLocs.add(east);
-			
-		List<Location> west = new ArrayList<Location>();
-		west.add(new Location(this.getRow(),this.getCol()-1));
-		moveLocs.add(west);
-		
-		List<Location> southEast = new ArrayList<Location>();
-		southEast.add(new Location(this.getRow()+1,this.getCol()+1));
-		moveLocs.add(southEast);
-		
-		List<Location> northEast = new ArrayList<Location>();
-		northEast.add(new Location(this.getRow()-1,this.getCol()+1));
-		moveLocs.add(northEast);
-		
-		List<Location> northWest = new ArrayList<Location>();
-		northWest.add(new Location(this.getRow()-1,this.getCol()-1));
-		moveLocs.add(northWest);
-		
-		List<Location> southWest = new ArrayList<Location>();
-		northWest.add(new Location(this.getRow()+1,this.getCol()-1));
-		moveLocs.add(southWest);
-		return moveLocs;
-	}
+      List<Location> north = new ArrayList<Location>();
+      north.add(location.getAdjacentLocation(Location.NORTH));
+      moveLocs.add(north);
 
-	@Override
-	public char getTeam() {
-		// TODO Auto-generated method stub
-		return team;
-	}
+      List<Location> south = new ArrayList<Location>();
+      south.add(location.getAdjacentLocation(Location.SOUTH));
+      moveLocs.add(south);
+
+      List<Location> east = new ArrayList<Location>();
+      east.add(location.getAdjacentLocation(Location.EAST));
+      moveLocs.add(east);
+
+      List<Location> west = new ArrayList<Location>();
+      west.add(location.getAdjacentLocation(Location.WEST));
+      moveLocs.add(west);
+
+      List<Location> southEast = new ArrayList<Location>();
+      southEast.add(location.getAdjacentLocation(Location.SOUTH_EAST));
+      moveLocs.add(southEast);
+
+      List<Location> northEast = new ArrayList<Location>();
+      northEast.add(location.getAdjacentLocation(Location.NORTH_EAST));
+      moveLocs.add(northEast);
+
+      List<Location> northWest = new ArrayList<Location>();
+      northWest.add(location.getAdjacentLocation(Location.NORTH_WEST));
+      moveLocs.add(northWest);
+
+      List<Location> southWest = new ArrayList<Location>();
+      northWest.add(location.getAdjacentLocation(Location.SOUTH_WEST));
+      moveLocs.add(southWest);
+
+      return moveLocs;
+   }
 
 }
