0c9d8cd5ab55ece46d93a9ac416e3835b2dc364d
Matthew Mahnke <mmahnke18@gmail.com>
2014-04-10 23:36:09 -0500

    Started creating the Pawn piece

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,73 @@
+package chess.pieces;
+
+import chess.Location;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pawn extends Piece {
+
+   private boolean isFirstMove;
+
+   /**
+    * Creates a pawn piece with the location of ({@code row},{@code col}) on {@code team} team.
+    *
+    * @param row  the row of this piece on the board
+    * @param col  the column of this piece on the board
+    * @param team the team this piece belongs to
+    */
+   public Pawn(int row, int col, char team) {
+      super(row, col);
+      this.team = team;
+      switch(team) {
+         case 'b':
+            icon = new ImageIcon("images/pawn_black.png");
+            break;
+         case 'w':
+            icon = new ImageIcon("images/pawn_white.png");
+            break;
+         default:
+            throw new IllegalArgumentException("Team must be 'b' for black team or 'w' for white team.");
+      }
+      this.isFirstMove = true;
+   }
+
+   /**
+    * Marks the pawn as having already moved, so it cannot move two spaces forward.
+    */
+   public void hasMoved() {
+      isFirstMove = false;
+   }
+
+   /**
+    * Returns all possible move locations of this piece.
+    *
+    * @return all possible move location of this piece
+    */
+   @Override
+   public List<List<Location>> getMoveLocations() {
+      int row = getRow();
+      int col = getCol();
+      List<List<Location>> moveLocs = new ArrayList<List<Location>>();
+
+      List<Location> vertical = new ArrayList<>();
+      if(team == 'w') {
+         vertical.add(location.getAdjacentLocation(Location.NORTH));
+         if(isFirstMove)
+            vertical.add(location.getAdjacentLocation(Location.NORTH).getAdjacentLocation(Location.NORTH));
+      } else {
+         vertical.add(location.getAdjacentLocation(Location.SOUTH));
+         if(isFirstMove)
+            vertical.add(location.getAdjacentLocation(Location.SOUTH).getAdjacentLocation(Location.SOUTH));
+      }
+
+      // TODO diagonal capture
+      List<Location> diagLeft = new ArrayList<>();
+      // TODO Pawn needs access to what pieces are where on the game board to decide if it can move diagonally
+
+      moveLocs.add(vertical);
+
+      return moveLocs;
+   }
+}
