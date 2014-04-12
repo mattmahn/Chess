5d36eeb7a37630aa4fa122441d07a1dc56504879
Matthew Mahnke <mmahnke18@gmail.com>
2014-04-11 21:24:12 -0500

    Code reformatting

--- src/chess/PieceButton.java
+++ src/chess/PieceButton.java
@@ -8,46 +8,49 @@ import java.awt.*;
 
 public class PieceButton extends JButton {
 
-	private Piece housedPiece;
-	private boolean focused,active;
+   private Piece housedPiece;
+   private boolean focused, active;
 
-	public PieceButton() {
-		super();
-	}
+   public PieceButton() {
+      super();
+   }
 
-	public Piece setPiece(Piece p) {
-		Piece oldPiece = housedPiece;
-		housedPiece = p;
-		if (p == null) {
-			this.setIcon(null);
-			this.setDisabledIcon(null);
-			return null;
-		} else {
-			this.setIcon(p.getIcon());
-			this.setDisabledIcon(p.getIcon());
-			return oldPiece;
-		}
-	}
-	public boolean isActive(){
-		return active;
-	}
-	public void setActive(boolean b){
-		this.active=b;
-	}
-	public Piece getPiece() {
-		return housedPiece;
-	}
+   public Piece setPiece(Piece p) {
+      Piece oldPiece = housedPiece;
+      housedPiece = p;
+      if(p == null) {
+         this.setIcon(null);
+         this.setDisabledIcon(null);
+         return null;
+      } else {
+         this.setIcon(p.getIcon());
+         this.setDisabledIcon(p.getIcon());
+         return oldPiece;
+      }
+   }
 
-	public void setFocused(boolean b) {
-		this.focused = b;
-		if (b)
-			this.setBackground(Color.yellow);
-		else
-			this.setBackground(null);
+   public boolean isActive() {
+      return active;
+   }
 
-	}
+   public void setActive(boolean b) {
+      this.active = b;
+   }
 
-	public boolean isFocused() {
-		return focused;
-	}
+   public Piece getPiece() {
+      return housedPiece;
+   }
+
+   public boolean isFocused() {
+      return focused;
+   }
+
+   public void setFocused(boolean b) {
+      this.focused = b;
+      if(b)
+         this.setBackground(Color.yellow);
+      else
+         this.setBackground(null);
+
+   }
 }
