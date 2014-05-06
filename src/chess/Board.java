3543f4d5dfffd429386ebf4c668c118bd038b214
longatoj <longatoj@msoe.edu>
2014-05-05 19:41:29 -0500

    Added java doc to Board.java and ChessLogic.java

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -20,8 +20,14 @@ public class Board extends JFrame {
 	/**
 	 * The PieceButtons that are placed onto the board.
 	 */
-	private ArrayList<ArrayList<PieceButton>> board = new ArrayList<ArrayList<PieceButton>>();
-	private PieceButton btnToMovePieceFrom = null;
+	private ArrayList<ArrayList<PieceButton>> buttons = new ArrayList<ArrayList<PieceButton>>();
+	/**
+	 * The button that was last clicked by the user
+	 */
+	private PieceButton btnLastClicked = null;
+	/**
+	 * The chess logic that dictates who's move it is and when the game is over
+	 */
 	private ChessLogic CL;
 
 	/**
@@ -37,7 +43,7 @@ public class Board extends JFrame {
 	}
 
 	/**
-	 * Creates a board that is ready for gameplay. <br>
+	 * <p>Creates a board that is ready for game play. 
 	 * This board has all pieces set in their appropriate starting positions and
 	 * awaits for the user to make the first move.
 	 */
@@ -47,23 +53,28 @@ public class Board extends JFrame {
 	}
 
 	/**
-	 * Creates a new board
+	 * Places buttons inside of the board and gives all buttons listeners
 	 */
 	private void initBtns() {
-		ActionListener testPieceListener = new ActionListener() {
+		ActionListener focusListener = new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent event) {
+				//Determine button clicked
 				PieceButton btnClicked = findbtn(event);
+				//Check if button was active prior to click
 				if (btnClicked.isActive()) {
 					if (isOccupied(btnClicked)) {
+						//If piece is focused remove focus 
 						if (btnClicked.isFocused()) {
 							btnClicked.setFocused(false);
-							btnToMovePieceFrom = null;
+							btnLastClicked = null;
 							removeFocuses();
-						} else {
+						}
+						//Button clicked is now in focus for player
+						else {
 							removeFocuses();
-							btnToMovePieceFrom = btnClicked;
+							btnLastClicked = btnClicked;
 							btnClicked.setFocused(true);
 							getValidLocations(btnClicked.getPiece());
 						}
@@ -73,34 +84,37 @@ public class Board extends JFrame {
 			}
 
 		};
-		ActionListener testMoveListener = new ActionListener() {
+		ActionListener moveListener = new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				//Determine button clicked
 				PieceButton btnToMoveTo = findbtn(e);
-				if (btnToMoveTo.isFocused() && !(btnToMoveTo.equals(btnToMovePieceFrom))) {
+				//If the button is one of the focused ones and is not the previously clicked button
+				if (btnToMoveTo.isFocused() && !(btnToMoveTo.equals(btnLastClicked))) {
 					movePieceToBtn(btnToMoveTo);
 				}
 			}
 
 		};
+		//Add new Piece buttons to each position in the board and add listeners
 		for (int row = 0; row < 8; row++) {
-			board.add(new ArrayList<PieceButton>());
+			buttons.add(new ArrayList<PieceButton>());
 			for (int col = 0; col < 8; col++) {
-				board.get(row).add(new PieceButton());
-				board.get(row).get(col).addActionListener(testPieceListener);
-				board.get(row).get(col).addActionListener(testMoveListener);
+				buttons.get(row).add(new PieceButton());
+				buttons.get(row).get(col).addActionListener(focusListener);
+				buttons.get(row).get(col).addActionListener(moveListener);
 			}
 		}
 	}
 
 	/**
-	 * Adds empty PieceButtons to the board. <br>
+	 * Adds buttons to the GUI.
 	 */
 	private void addBtnsToBoard() {
 		JPanel mainContent = new JPanel();
 		mainContent.setLayout(new GridLayout(8, 8));
-		for (ArrayList<PieceButton> row : board) {
+		for (ArrayList<PieceButton> row : buttons) {
 			for (int col = 0; col < row.size(); col++) {
 				mainContent.add(row.get(col));
 			}
@@ -118,19 +132,18 @@ public class Board extends JFrame {
 	protected PieceButton findbtn(ActionEvent event) {
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
-				if (board.get(row).get(col).equals(event.getSource()))
-					return board.get(row).get(col);
+				if (buttons.get(row).get(col).equals(event.getSource()))
+					return buttons.get(row).get(col);
 			}
 		}
 		return null;
 	}
 
 	/**
-	 * Checks if the passed Button is occupied by a piece by checking if the
-	 * button has an Icon.
+	 * Checks if the passed Button is occupied by a piece.
 	 *
 	 * @param location
-	 *            JButton being checked
+	 *            PieceButton being checked
 	 * @return True if location has icon.
 	 */
 	public boolean isOccupied(PieceButton location) {
@@ -138,14 +151,13 @@ public class Board extends JFrame {
 	}
 
 	/**
-	 * Removes the highlighting and the focus attribute of all buttons are set
-	 * to false
+	 * Removes the focuses from all buttons on the board
 	 */
 	private void removeFocuses() {
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
-				board.get(row).get(col).setFocused(false);
-				board.get(row).get(col).setBackground(null);
+				buttons.get(row).get(col).setFocused(false);
+				buttons.get(row).get(col).setBackground(null);
 			}
 		}
 
@@ -185,7 +197,11 @@ public class Board extends JFrame {
 		}
 
 	}
-
+	/**
+	 * Helper method for getValidLocations that is called when a pawn is about to move
+	 * @param currentLocation Location of the pawn
+	 * @param pawn Pawn trying to move
+	 */
 	private void checkPawnLocation(Location currentLocation, Pawn pawn) {
 		Location pawnLoc = pawn.getLocation();
 		boolean isNorth = pawnLoc.getAdjacentLocation(Location.NORTH).equals(currentLocation);
@@ -201,15 +217,20 @@ public class Board extends JFrame {
 				getButtonAtLocation(currentLocation).setFocused(true);
 		}
 	}
-
+	/**
+	 * <p>Moves the piece that was on the last button clicked to the new PieceButton that is
+	 * passed to this method. After moving the piece to the new button the button last clicked
+	 * no longer contains the piece that has moved.
+	 * @param btnToMoveTo PieceButton that will recieve the new piece
+	 */
 	protected void movePieceToBtn(PieceButton btnToMoveTo) {
-		if (btnToMovePieceFrom != null) {
-			if(btnToMovePieceFrom.getPiece() instanceof Pawn){
-				Pawn pawn = (Pawn) btnToMovePieceFrom.getPiece();
+		if (btnLastClicked != null) {
+			if(btnLastClicked.getPiece() instanceof Pawn){
+				Pawn pawn = (Pawn) btnLastClicked.getPiece();
 				pawn.hasMoved();
 			}
-			Piece pieceToMove = btnToMovePieceFrom.getPiece();
-			btnToMovePieceFrom.setPiece(null);
+			Piece pieceToMove = btnLastClicked.getPiece();
+			btnLastClicked.setPiece(null);
 			Piece pieceRemoved = btnToMoveTo.setPiece(pieceToMove);
 			CL.removePiece(pieceRemoved);
 			Location locOfBtnToMoveTo = findLocationOfButton(btnToMoveTo);
@@ -218,7 +239,13 @@ public class Board extends JFrame {
 			CL.updateLogic();
 		}
 	}
-
+	/**
+	 *<p>Determines if the location is valid based on the board.
+	 * A location is valid as long as its Row and Column is within the bounds of
+	 * the grid.
+	 * @param currentLocation Location to test
+	 * @return True if location is valid false otherwise
+	 */
 	public static boolean isValidLocationOnGird(Location currentLocation) {
 		int col = currentLocation.getCol();
 		int row = currentLocation.getRow();
@@ -232,22 +259,26 @@ public class Board extends JFrame {
 	 *
 	 * @param loc
 	 *            The location to pull the button from
-	 * @return PieceButton in the location requested
+	 * @return PieceButton in the location requested, null if Loc is not valid
 	 */
 	public PieceButton getButtonAtLocation(Location loc) {
 		PieceButton btn = null;
 		try {
-			btn = board.get(loc.getRow()).get(loc.getCol());
+			btn = buttons.get(loc.getRow()).get(loc.getCol());
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Requested location inside of getButtonAtLocation() was null");
 		}
 		return btn;
 	}
-
+	/**
+	 * Returns the location where the button is located at in the grid
+	 * @param btn Button to find location of
+	 * @return Location of the button
+	 */
 	private Location findLocationOfButton(PieceButton btn) {
 		for (int row = 0; row < 8; row++) {
 			for (int col = 0; col < 8; col++) {
-				PieceButton current = board.get(row).get(col);
+				PieceButton current = buttons.get(row).get(col);
 				if (current.equals(btn))
 					return new Location(row, col);
 			}
@@ -261,7 +292,7 @@ public class Board extends JFrame {
 	 * @return The list of all PieceButtons within the board
 	 */
 	public ArrayList<ArrayList<PieceButton>> getBoard() {
-		return board;
+		return buttons;
 	}
 
 	/**
@@ -271,7 +302,7 @@ public class Board extends JFrame {
 	 *            Piece that will be placed on the board.
 	 */
 	public void placePiece(Piece piece) {
-		board.get(piece.getRow()).get(piece.getCol()).setPiece(piece);
+		buttons.get(piece.getRow()).get(piece.getCol()).setPiece(piece);
 
 	}
 
--- src/chess/ChessLogic.java
+++ src/chess/ChessLogic.java
@@ -4,19 +4,48 @@ import java.util.ArrayList;
 
 import chess.pieces.*;
 import chess.pieces.Rook;
-
+/**
+ * <p>This class monitors the state of the game by organizing al lthe pieces into
+ * a list. It also dictates who's turn it is and when the player may interact with
+ * pieces and what pieces they may interact with.
+ * @author Jordan Longato,Matthew Mahnke
+ *
+ */
 public class ChessLogic {
-
+	/**
+	 * Board used for game play.
+	 */
 	private Board ChessBoard;
+	/**
+	 * A list of the pieces that the Black player has left
+	 */
 	private ArrayList<Piece> BlackPieces;
+	/**
+	 * A list of the pieces that the White player has left
+	 */
 	private ArrayList<Piece> WhitePieces;
+	/**
+	 * Variable that determines whose turn it is.
+	 */
 	private boolean isWhitesTurn = true;
-
+	/**
+	 * <p>Produces a Chess Logic that will maintain the gameplay
+	 * of Chess so long as after each action done to the board 
+	 * is followed by a call to this class's appropriate method for
+	 * what action happened on the board.
+	 * <p>For example if a piece over takes a piece on the board then
+	 * a call must be made to the removePiece method and then to the updateLogic
+	 * method.
+	 * @param board Board to maintain
+	 */
 	public ChessLogic(Board board) {
 		ChessBoard = board;
 		placeNewPeices();
 	}
-
+	/**
+	 * Places a new set of white and black Chess pieces onto the board
+	 * and adds them to the board.
+	 */
 	private void placeNewPeices() {
 		// TODO Auto-generated method stub
 		initPieces();
@@ -25,42 +54,49 @@ public class ChessLogic {
 		for (Piece piece : WhitePieces)
 			ChessBoard.placePiece(piece);
 	}
-
+	/**
+	 * Initializes a new set of pieces for white and black
+	 */
 	private void initPieces() {
 		BlackPieces = new ArrayList<Piece>();
 		WhitePieces = new ArrayList<Piece>();
 		// Add pawns
-		for (int i = 0; i < 8; i++){
+		for (int i = 0; i < 8; i++) {
 			BlackPieces.add(new Pawn(1, i, 'b'));
 			WhitePieces.add(new Pawn(6, i, 'w'));
 		}
-		//Add rooks
-		BlackPieces.add(new Rook(0,0,'b'));
-		BlackPieces.add(new Rook(0,7,'b'));
-		WhitePieces.add(new Rook(7,0,'w'));
-		WhitePieces.add(new Rook(7,7,'w'));
-		//Add Knights
-		BlackPieces.add(new Knight(0,1,'b'));
-		BlackPieces.add(new Knight(0,6,'b'));
-		WhitePieces.add(new Knight(7,1,'w'));
-		WhitePieces.add(new Knight(7,6,'w'));
-		//Add bishop
-		BlackPieces.add(new Bishop(0,2,'b'));
-		BlackPieces.add(new Bishop(0,5,'b'));
-		WhitePieces.add(new Bishop(7,2,'w'));
-		WhitePieces.add(new Bishop(7,5,'w'));
-		//Add Queen
-		BlackPieces.add(new Queen(0,3,'b'));
-		WhitePieces.add(new Queen(7,3,'w'));
-		//Add kings
+		// Add rooks
+		BlackPieces.add(new Rook(0, 0, 'b'));
+		BlackPieces.add(new Rook(0, 7, 'b'));
+		WhitePieces.add(new Rook(7, 0, 'w'));
+		WhitePieces.add(new Rook(7, 7, 'w'));
+		// Add Knights
+		BlackPieces.add(new Knight(0, 1, 'b'));
+		BlackPieces.add(new Knight(0, 6, 'b'));
+		WhitePieces.add(new Knight(7, 1, 'w'));
+		WhitePieces.add(new Knight(7, 6, 'w'));
+		// Add bishop
+		BlackPieces.add(new Bishop(0, 2, 'b'));
+		BlackPieces.add(new Bishop(0, 5, 'b'));
+		WhitePieces.add(new Bishop(7, 2, 'w'));
+		WhitePieces.add(new Bishop(7, 5, 'w'));
+		// Add Queen
+		BlackPieces.add(new Queen(0, 3, 'b'));
+		WhitePieces.add(new Queen(7, 3, 'w'));
+		// Add kings
 		BlackPieces.add(new King(0, 4, 'b'));
 		WhitePieces.add(new King(7, 4, 'w'));
 		// Disable Blacks Pieces because of new game
 		enablePieces(BlackPieces, false);
-		//Enable White Pieces because of new game
-		enablePieces(WhitePieces,true);
+		// Enable White Pieces because of new game
+		enablePieces(WhitePieces, true);
 	}
-
+	/**
+	 * Updates the logic of this class by checking if there 
+	 * a player has won the game. If a player has not won
+	 * the game then this class will make the opposite players 
+	 * pieces become available for their turn.
+	 */
 	public void updateLogic() {
 		// Check for win
 		if (checkBlackWin()) {
@@ -82,7 +118,11 @@ public class ChessLogic {
 		}
 
 	}
-
+	/**
+	 * Checks if white has won by checking to see if
+	 * there is still a king in the list of pieces.
+	 * @return True if no king was found in pieces false otherwise
+	 */
 	private boolean checkWhiteWin() {
 		for (Piece piece : BlackPieces) {
 			if (piece instanceof King)
@@ -90,7 +130,11 @@ public class ChessLogic {
 		}
 		return true;
 	}
-
+	/**
+	 * Checks if black has won by checking to see if 
+	 * there is still a king in the list of pieces.
+	 * @return True if no king was found in pieces false otherwise. 
+	 */
 	private boolean checkBlackWin() {
 		for (Piece piece : WhitePieces) {
 			if (piece instanceof King)
@@ -98,19 +142,29 @@ public class ChessLogic {
 		}
 		return true;
 	}
-
+	/**
+	 * Enables the list of pieces based on enable
+	 * @param Pieces Pieces to enable
+	 * @param enable 
+	 */
 	private void enablePieces(ArrayList<Piece> Pieces, boolean enable) {
 		for (Piece piece : Pieces) {
-			PieceButton a = ChessBoard.getButtonAtLocation(new Location(piece
-					.getRow(), piece.getCol()));
+			PieceButton a = ChessBoard.getButtonAtLocation(new Location(piece.getRow(), piece
+					.getCol()));
 			a.setActive(enable);
 		}
 
 	}
-
+	/**
+	 * Checks to see if there a chance that a piece will over take another piece
+	 * by moving onto a spot. 
+	 * @param pieceMoving Piece trying to move
+	 * @param currentLocation Location that piece wants to move to but 
+	 * there is a piece in the way
+	 * @return true if pieceMoving can overtake the piece false otherwise
+	 */
 	public boolean potentialKill(Piece pieceMoving, Location currentLocation) {
-		PieceButton btnKillLocation = ChessBoard
-				.getButtonAtLocation(currentLocation);
+		PieceButton btnKillLocation = ChessBoard.getButtonAtLocation(currentLocation);
 		char pieceTeam = pieceMoving.getTeam();
 		// Check what team the piece attempting to move is on
 		if (pieceTeam == 'w') {
@@ -126,7 +180,10 @@ public class ChessLogic {
 				return false;
 		}
 	}
-
+	/**
+	 * Removes a piece from the appropriate list of pieces
+	 * @param pieceRemoved Piece to be removed
+	 */
 	public void removePiece(Piece pieceRemoved) {
 		if (pieceRemoved != null) {
 			for (int i = 0; i < BlackPieces.size(); i++) {
